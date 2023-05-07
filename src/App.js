fde16d83bb94207c0220ccc1175130036f93bb6f
amr <aa5473@fayoum.edu.eg>
2023-05-07 14:59:20 +0300

    i have just finished my old version , Smile

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/header/Header";
 import Home from "./pages/home/home";
 import MovieList from "./components/movieList/movieList";
@@ -11,7 +10,7 @@ function App() {
       <Router>
         <Header />
         <Routes>
-          <Route path="/IMDB-CLONE/" index element={<Home />}></Route>
+          <Route path="/" index element={<Home />}></Route>
           <Route path="movie/:id" element={<Movie />}></Route>
           <Route path="movies/:type" element={<MovieList />}></Route>
           <Route path="/*" element={<h1>Error Page</h1>}></Route>
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,18 +1,75 @@
-import React from "react"
-import "./Header.css"
-import { Link } from "react-router-dom"
+import React, { useState } from "react";
+import "./Header.css";
+import { Link } from "react-router-dom";
+import { RiMovie2Fill } from "react-icons/ri";
 
 const Header = () => {
-    return (
-        <div className="header">
-            <div className="headerLeft">
-                <Link to="/"><img className="header__icon" src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/IMDB_Logo_2016.svg/2560px-IMDB_Logo_2016.svg.png" /></Link>
-                <Link to="/movies/popular" style={{textDecoration: "none"}}><span>Popular</span></Link>
-                <Link to="/movies/top_rated" style={{textDecoration: "none"}}><span>Top Rated</span></Link>
-                <Link to="/movies/upcoming" style={{textDecoration: "none"}}><span>Upcoming</span></Link>
-            </div>
+  const [first, setFirst] = useState(false);
+  return (
+    <div className="header">
+      <div>
+        <Link to="/">
+          <img
+            alt=""
+            className="header__icon"
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/IMDB_Logo_2016.svg/2560px-IMDB_Logo_2016.svg.png"
+          />
+        </Link>
+      </div>
+      <div className="headerLeft">
+        <Link to="/" style={{ textDecoration: "none" }}>
+          <span>Home</span>
+        </Link>
+        <Link to="/movies/popular" style={{ textDecoration: "none" }}>
+          <span>Popular</span>
+        </Link>
+        <Link to="/movies/top_rated" style={{ textDecoration: "none" }}>
+          <span>Top Rated</span>
+        </Link>
+        <Link to="/movies/upcoming" style={{ textDecoration: "none" }}>
+          <span>Upcoming</span>
+        </Link>
+      </div>
+      <div className="headerList">
+        <div
+          className="icon"
+          onClick={() => setFirst((prevState) => !prevState)}
+        >
+          <RiMovie2Fill />
         </div>
-    )
-}
+        <div className={`headerListItems ${first ? "active" : "inActive"}`}>
+          <Link
+            onClick={() => setFirst((prevState) => !prevState)}
+            to="/"
+            style={{ textDecoration: "none" }}
+          >
+            <span>Home</span>
+          </Link>
+          <Link
+            onClick={() => setFirst((prevState) => !prevState)}
+            to="/movies/popular"
+            style={{ textDecoration: "none" }}
+          >
+            <span>Popular</span>
+          </Link>
+          <Link
+            onClick={() => setFirst((prevState) => !prevState)}
+            to="/movies/top_rated"
+            style={{ textDecoration: "none" }}
+          >
+            <span>Top Rated</span>
+          </Link>
+          <Link
+            onClick={() => setFirst((prevState) => !prevState)}
+            to="/movies/upcoming"
+            style={{ textDecoration: "none" }}
+          >
+            <span>Upcoming</span>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
