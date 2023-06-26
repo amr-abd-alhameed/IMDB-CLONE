4049e81dc15d0e4eb0d36764c98d63f68b4558c0
amr <aa5473@fayoum.edu.eg>
2023-06-26 23:21:38 +0300

    persist data in local storage using redux persist

--- src/Redux Toolkit/Store/Store.js
+++ src/Redux Toolkit/Store/Store.js
@@ -1,12 +1,25 @@
 import { configureStore } from "@reduxjs/toolkit";
 import MovieSlice from "../Features/addMovie/movieSlice";
+import { persistStore, persistReducer } from "redux-persist";
+import thunk from "redux-thunk";
+import storage from "redux-persist/lib/storage";
+
+const persistConfig = {
+  key: "movie",
+  storage,
+};
+const persistedReducer = persistReducer(persistConfig, MovieSlice);
 
 export const Store = configureStore({
   reducer: {
-    movies: MovieSlice,
+    movies: persistedReducer,
     //     [movieApi.reducerPath]: movieApi.reducer,
     //   },
     //   middleware: (getDefaultMiddleware) =>
     //     getDefaultMiddleware().concat(movieApi.middleware),
   },
+  middleware: [thunk],
 });
+const persistor = persistStore(Store);
+export { persistor };
+export default Store;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
-import React from "react";
-import "./index.css";
+import React from "react";import "./index.css";
 import App from "./App";
 import { createRoot } from "react-dom/client";
-import { Store } from "./Redux Toolkit/Store/Store";
+import { Store, persistor } from "./Redux Toolkit/Store/Store";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
 
 root.render(
   <Provider store={Store}>
-    <App />
+    <PersistGate persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>
 );
